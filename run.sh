f0a075b970ca95aef7004e1118eefe7db5bfb0f3
Andrei Korneev <a.korneev1@gmail.com>
2020-05-14 00:26:48 +0200

    Updated on save/restore/strees

--- run.sh
+++ run.sh
@@ -1,5 +1,17 @@
 #DEBUG_FLAGS=(-g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -DLOCAL -rdynamic)
+MAIN_TIMEOUT="2s"
 DEBUG_FLAGS=(-g -fsanitize=address -fsanitize=undefined -DLOCAL -rdynamic)
+STRESS_TEST_COUNT=10000
+STRESS_TEST_OUTPUT_EACH=1000
+STRESS_FAIL_ON_INCORRECT_TEST=true
+
+strip_debug_code() {
+  if command -v unifdef >/dev/null 2>&1; then
+    unifdef -ULOCAL $1 > $2
+  else
+    cat $1 > $2
+  fi
+}
 
 update_headers() {
   OUT_FILE="algs/all.h"
@@ -17,18 +29,18 @@ prepare_fs() {
 compile() {
   filename="$(basename -s ".cpp" $1)"
   ./translate/__main__.py "$1" "generated/${filename}.cpp"
-  unifdef -ULOCAL "generated/${filename}.cpp" > "generated/${filename}_no_debug.cpp"
+  strip_debug_code "generated/${filename}.cpp" > "generated/${filename}_no_debug.cpp"
   g++-9 -std=gnu++1z -I . -O2 -D LOCAL "generated/${filename}.cpp" -o "obj/${filename}.out"
 }
 
 run() {
   compile main.cpp
-  if ! timeout 2s obj/main.out < input.txt &> output.txt ; then
+  if ! timeout ${MAIN_TIMEOUT} obj/main.out < input.txt &> output.txt ; then
     echo "Failed; running in debug mode"
-    g++-9 -std=gnu++1z "${DEBUG_FLAGS[@]}" "generated/main.cpp" -o "obj/main.out"
-    timeout 2s obj/main.out < input.txt &> output.txt
+    g++-9 -std=gnu++1z "${DEBUG_FLAGS[@]}" "generated/main.cpp" -o "obj/main_debug.out"
+    timeout ${MAIN_TIMEOUT} obj/main_debug.out < input.txt &> output.txt
   else
-    unifdef -ULOCAL generated/main.cpp | pbcopy
+    cat "generated/main_no_debug.cpp" | pbcopy
     echo "Done"
   fi
 }
@@ -39,21 +51,33 @@ stress() {
   compile stress/generator.cpp
   compile stress/checker.cpp
 
-  for i in {1..1000}; do
-    if (($i % 50 == 0)); then
+  for (( i=1; i<=${STRESS_TEST_COUNT}; i++ )); do
+    if (($i % ${STRESS_TEST_OUTPUT_EACH} == 0)); then
       echo "Test #$i"
     fi
-    timeout 2s obj/generator.out > input.txt 2> /dev/null
-    timeout 2s obj/main.out < input.txt > output.txt 2> /dev/null
-    timeout 2s obj/slow.out < input.txt > stress/output.txt 2> /dev/null
+    timeout 2s obj/generator.out $i > input.txt 2> /dev/null
+    if ! timeout 2s obj/slow.out < input.txt > stress/output.txt 2> /dev/null; then
+      echo "Incorrect test #$i"
+      if [ "${STRESS_FAIL_ON_INCORRECT_TEST}" = true ] ; then
+        return
+      else
+        continue
+      fi
+    fi
+    timeout ${MAIN_TIMEOUT} obj/main.out < input.txt > output.txt 2> /dev/null
     if ! cmp -s output.txt stress/output.txt > /dev/null 2>&1; then
-      echo "Diff found. Test #$i"
-      return
+      if ! timeout 2s obj/checker.out input.txt stress/output.txt output.txt  &> stress/checker.txt; then
+        echo "Diff found. Test #$i"
+        cat stress/checker.txt
+        return;
+      fi
     fi
   done
   echo "No diff found"
 }
 
+# Example of usage both: external graph generator and custom generator.
+# [jngen](https://github.com/ifsmirnov/jngen) - super useful generators for stress testing.
 tmp() {
   compile main.cpp
   compile stress/generator.cpp
@@ -82,6 +106,12 @@ save() {
   mkdir -p $1
   cp -r stress "$1/"
   cp main.cpp "$1/"
+  cp input.txt "$1/"
+  cp output.txt "$1/"
+}
+
+restore() {
+  cp -r "$1/" .
 }
 
 new() {
@@ -106,8 +136,14 @@ main() {
     save)
       save $2
       ;;
+    restore)
+      restore $2
+      ;;
+    compile)
+      compile $2
+      ;;
     *)
-      echo "Usage: $0 {run|new|save|stress}"
+      echo "Usage: $0 {run|new|compile|save|restore|stress}"
       exit 1
       ;;
   esac
